92e298a37b3731d8be0ea84525b61c1860605cc8
terabvte <marcofediuc@gmail.com>
2025-05-27 13:02:22 +0200

    Forgor to add files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/pokemon",
+        destination:
+          "https://r5d2yrnr91.execute-api.eu-west-2.amazonaws.com/Prod/pokemon",
+      },
+      {
+        source: "/api/pokemon/:id",
+        destination:
+          "https://r5d2yrnr91.execute-api.eu-west-2.amazonaws.com/Prod/pokemon/:id",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
